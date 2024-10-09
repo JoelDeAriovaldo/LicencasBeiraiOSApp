75a6c50c9c9f728a011c9f9cea2d62576afb11b4
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-10-09 20:50:17 +0200

    Add initial project files and structure

---
+++ LicencasBeiraiOSApp/Views/Payments/PaymentConfirmationView.swift
@@ -0,0 +1,9 @@
+//
+//  PaymentConfirmationView.swift
+//  LicencasBeiraiOSApp
+//
+//  Created by Equip Mozambique on 10/9/24.
+//  Copyright © 2024 Joel De Ariovaldo. All rights reserved.
+//
+
+import Foundation
